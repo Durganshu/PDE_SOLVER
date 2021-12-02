4adf3f4cb597387f3a270541f4489be9210f820e
Durganshu <durganshu.mishra@tum.de>
2021-12-02 18:05:56 +0100

    Added dummy function calls and intialized main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<vector>
 
-std::vector<std::vector<double>> five_point_stencil();
+std::vector<std::vector<double>> five_point_stencil(std::vector<std::vector<double>>);
 
-std::vector<std::vector<double>> eight_point_stencil();
+std::vector<std::vector<double>> eight_point_stencil(std::vector<std::vector<double>>);
+
+std::vector<std::vector<double>> set_boundary_conditions(std::vector<std::vector<double>>);
 
 bool unit_test();
 
@@ -11,7 +13,24 @@ void write_results();
 
 int main()
 {
-    std::cout<<"Commit Testing"<<std::endl;
+    int nx, ny = 100;
+    std::vector<std::vector<double>> temperature;
+    temperature.resize(nx);
+    for(size_t i =0;i<nx;i++){
+        temperature[i].resize(ny);
+    }
+
+    temperature = set_boundary_conditions(temperature);
+
+    std::vector<std::vector<double>> five_point = five_point_stencil(temperature);
+
+    std::vector<std::vector<double>> eight_point = eight_point_stencil(temperature);
+
+    {
+
+        //This part calculates the final temperature
+    }
+
     
 
     return 0;
