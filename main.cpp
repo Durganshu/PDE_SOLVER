838b7923b9ce905a78b9e31e4f0bf295dc665c84
Durganshu <durganshu.mishra@tum.de>
2021-12-08 18:44:16 +0100

    Replaced L and H with nx and ny; Other nominal restructuring

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 void set_boundary_conditions(std::vector<std::vector<double>>& temperature);
 
-void five_point_stencil(std::vector<std::vector<double>>& temperature);
+void four_point_stencil(std::vector<std::vector<double>>& temperature);
 
 void eight_point_stencil(std::vector<std::vector<double>>& temperature);
 
@@ -52,14 +52,14 @@ int main()
     //print_grid(temperature);
 
     std::cout<<"How do you want to solve the problem?"<<std::endl;
-    std::cout<<"Press 1 for Five Point Stencil"<<std::endl;
+    std::cout<<"Press 1 for Four Point Stencil"<<std::endl;
     std::cout<<"Press 2 for Eight Point Stencil"<<std::endl;
     std::cout<<"Press 3 for running a unit test"<<std::endl;
     
     int input;
     std::cin>>input;
     if(input == 1){
-        five_point_stencil(temperature);
+        four_point_stencil(temperature);
         write_results(x_values,y_values,temperature);
     }
 
@@ -68,7 +68,7 @@ int main()
         write_results(x_values,y_values,temperature);
     }
     else if(input == 3){
-        std::cout<<"Press a for Five Point Stencil"<<std::endl;
+        std::cout<<"Press a for Four Point Stencil"<<std::endl;
         std::cout<<"Press b for Eight Point Stencil"<<std::endl;
         char choice;
         std::cin>>choice;
@@ -85,15 +85,15 @@ int main()
 
 void set_boundary_conditions(std::vector<std::vector<double>> &temperature)
 {
-    int H=temperature.size();
-    int L=temperature[0].size();
+    int nx=temperature.size();
+    int ny=temperature[0].size();
 
     //std::cout<<H<<std::endl;
     //std::cout<<L<<std::endl;
 
-    for (auto i=0;i<H;i++){        
+    for (auto i=0;i<nx;i++){        
         temperature[i][0]=0;
-        temperature[i][L-1]=1;
+        temperature[i][ny-1]=1;
     }
     //print_grid(temperature);
     //std::cout<<std::endl;
@@ -106,28 +106,28 @@ void set_boundary_conditions(std::vector<std::vector<double>> &temperature)
 
 void print_grid(const std::vector<std::vector<double>> &temperature)
 {
-    int H=temperature.size();
-    int L=temperature[0].size();
+    int nx=temperature.size();
+    int ny=temperature[0].size();
 
-    for (int i=0;i<H;i++){
-        for (int j=0; j<L; j++){
+    for (int i=0;i<nx;i++){
+        for (int j=0; j<ny; j++){
             std::cout<<temperature[i][j]<<" ";
         }
         std::cout<<"\n";
     }
 }
 
-void five_point_stencil(std::vector<std::vector<double>>& temperature){
+void four_point_stencil(std::vector<std::vector<double>>& temperature){
     std::cout<<"In progress........."<<std::endl;
-    int H = temperature.size();
-    int L = temperature[0].size();
+    int nx = temperature.size();
+    int ny = temperature[0].size();
 
     //Modifying the interior grid points at a particular iteration
     int num_iter=0;
     while(num_iter<5000){
         //std::cout<<"Iteration number "<<num_iter<<std::endl;
-        for(int i=1;i<(H-1);i++){
-            for(int j=1;j<(L-1);j++){
+        for(int i=1;i<(nx-1);i++){
+            for(int j=1;j<(ny-1);j++){
                 temperature[i][j]=(0.25)*(temperature[i][j+1] +
                     temperature[i-1][j]+temperature[i+1][j]+temperature[i][j-1]);
             }
@@ -136,21 +136,21 @@ void five_point_stencil(std::vector<std::vector<double>>& temperature){
         // print_grid(temperature);
         num_iter=num_iter+1;
     }
-    std::cout<<"Five point stencil implemented."<<std::endl;
+    std::cout<<"Four point stencil implemented."<<std::endl;
    
 }
 
 void eight_point_stencil(std::vector<std::vector<double>>& temperature){
     std::cout<<"In progress........."<<std::endl;
-    int H = temperature.size();
-    int L = temperature[0].size();
+    int nx = temperature.size();
+    int ny = temperature[0].size();
 
     //Modifying the interior grid points at a particular iteration
     int num_iter=0;
     while(num_iter<5000){
         //std::cout<<"Iteration number "<<num_iter<<std::endl;
-        for(int i=1;i<(H-1);i++){
-            for(int j=1;j<(L-1);j++){
+        for(int i=1;i<(nx-1);i++){
+            for(int j=1;j<(ny-1);j++){
                 temperature[i][j] = (1.0/8.0)*(temperature[i-1][j+1] + temperature[i][j+1] +
                     temperature[i+1][j+1] + temperature[i-1][j] + temperature[i+1][j] +
                     temperature[i-1][j-1] + temperature[i][j-1] + temperature[i+1][j-1]);
@@ -164,22 +164,24 @@ void eight_point_stencil(std::vector<std::vector<double>>& temperature){
 }
 
 bool unit_test(char choice){
-    int L = 100;
-    int H = 100;
+    int nx = 100;
+    int ny = 100;
     bool tests_passed = true;
 
     std::vector<std::vector<double>> temperature;
-    temperature.resize(L);
-    for(int i =0;i<L;i++){
-        temperature[i].resize(H);
+    temperature.resize(nx);
+    for(int i =0;i<nx;i++){
+        temperature[i].resize(ny);
     }
 
     set_boundary_conditions(temperature);
 
-    if(choice == 'a'){
-        five_point_stencil(temperature);
+    if(choice == 'a' || choice == 'A'){
+        std::cout<<"choice selected: a"<<std::endl;
+        four_point_stencil(temperature);
     }
-    else if(choice == 'b'){
+    else if(choice == 'b' || choice == 'B'){
+        std::cout<<"choice selected: b "<<std::endl;
         eight_point_stencil(temperature);
     }
     else{
@@ -190,7 +192,7 @@ bool unit_test(char choice){
     //Calculating analytical solution
     std::vector<double> x_values, y_values;
     double i = 0;
-    double diff = 1.0/(L-1);
+    double diff = 1.0/(nx-1);
     
     while(i<1){
         x_values.push_back(i);
@@ -203,28 +205,24 @@ bool unit_test(char choice){
     double dim_y = 1;
 
     std::vector<std::vector<double>> reference_temperature, temp_temperature;
-    reference_temperature.resize(H);
-    temp_temperature.resize(H);
-    for(int i=0;i<H;i++){
-        reference_temperature[i].resize(L);
-        temp_temperature[i].resize(L);
+    reference_temperature.resize(nx);
+    temp_temperature.resize(nx);
+    for(int i=0;i<nx;i++){
+        reference_temperature[i].resize(ny);
+        temp_temperature[i].resize(ny);
     }
 
     for(int n=1;n<=10000;n++){
         double coeff = (2.0*(1.0-cos(n*M_PI)))/((n*M_PI)*sinh((n*M_PI*dim_y)/dim_x));
         
-        for(int k=0;k<H;k++){
+        for(int k=0;k<nx;k++){
                 double hyperbolic_sine = sinh(((n*M_PI)/dim_x)*x_values[k]);
                 if(hyperbolic_sine> std::numeric_limits<double>::max()){
                     hyperbolic_sine = std::numeric_limits<double>::max();
                 }
                 double dummy_temp = (coeff)*(hyperbolic_sine);
-                if(isnan(dummy_temp)){
-                    std::cout<<((n*M_PI)/dim_x)*x_values[k]<<",";
-                    std::cout<<"\n";
-                }
                 
-                for(int l = 0;l<L;l++){
+                for(int l = 0;l<ny;l++){
                     temp_temperature[k][l] = dummy_temp*(sin(((n*M_PI)/dim_y)*y_values[l]));
                     reference_temperature[l][k] = reference_temperature[l][k] + temp_temperature[k][l];
                     //std::cout<<reference_temperature[l][k]<<",";
@@ -268,14 +266,14 @@ void write_results(const std::vector<double> x_values, const std::vector<double>
     std::cout<<"Writing results....."<<std::endl;
     std::ofstream myfile;
     myfile.open (filename);
-    int H=temperature.size();
-    int L=temperature[0].size();
+    int nx=temperature.size();
+    int ny=temperature[0].size();
     //std::cout<<"Size of reference solution: "<<reference_temperature[0][0]<<std::endl;
     if(reference_temperature.size() > 1){
         myfile<<"X"<<","<<"Y"<<","<<"Numerical Solution (in K)"<<","<<
-            "Analytical Solution"<<","<<"Absolute Error"<<std::endl;
-        for (int i=0;i<H;i++){
-            for (int j=0; j<L; j++){
+            "Analytical Solution (in K)"<<","<<"Absolute Error"<<std::endl;
+        for (int i=0;i<nx;i++){
+            for (int j=0; j<ny; j++){
                 double error = abs(reference_temperature[i][j] - temperature[i][j]);
                 myfile<<x_values[i]<<","<<y_values[j]<<","<<temperature[i][j]<<","
                 <<reference_temperature[i][j]<<","<<abs(reference_temperature[i][j] - temperature[i][j])    
@@ -292,8 +290,8 @@ void write_results(const std::vector<double> x_values, const std::vector<double>
         //std::cout<<"Came here!"<<std::endl;
         myfile<<"X"<<","<<"Y"<<","<<"Numerical Solution (in K)"<<std::endl;
         
-        for (int i=0;i<H;i++){
-            for (int j=0; j<L; j++){
+        for (int i=0;i<nx;i++){
+            for (int j=0; j<ny; j++){
                 myfile<<x_values[i]<<","<<y_values[j]<<","<<temperature[i][j]<<"\n";
                 //std::cout<<temperature[i][j]<<",";
             }
