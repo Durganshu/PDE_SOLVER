ba47cfc2c7a0e3cc6f07716d14361bde88b22e86
Durganshu <durganshu.mishra@tum.de>
2021-12-09 00:06:54 +0100

    Updated python files

--- plot.py
+++ plot.py
@@ -1,30 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
-data = np.genfromtxt('reference_results.csv', delimiter=',')
+
+# Enter 'results.csv' for plotting numerical solution
+# Enter 'unit_test_results.csv' for plotting analytical solution
+filename = 'unit_test_results.csv'
+data = np.genfromtxt(filename, delimiter=',')
 
 data1 = data[1:,:]
 
 L = 100
 H = 100
 
-row_values = range(0,L)
+row_values = range(0,100)
 
 col = 0
 itr = 0
-data2 = np.zeros((L, H))
+data2 = np.zeros((100, 100))
 
+
+column = 2
+if(filename == 'unit_test_results.csv'):
+    column = 3
+
+    
 while(1):
     for row in row_values:
-        data2[row,col] = data1[itr,2]
+        data2[row,col] = data1[itr,column]
         itr = itr + 1
     col = col+1
     if(col == 100):
         break
 
     
-plt.imshow(np.transpose(data2), extent=[-1, 1, -1, 1])
+plt.imshow(np.transpose(data2),cmap = cm.jet, extent=[0, 1, 0, 1])
 plt.colorbar()
 plt.show()
-plt.savefig("results.png")  #savefig, don't show
+
+#plt.savefig("results.png")  #savefig, don't show
